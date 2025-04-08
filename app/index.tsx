c41d4ab7620e5d9f1f2f9cd0cc9b522a3af73324
udaykiran <40133834+uday7981@users.noreply.github.com>
2025-04-08 11:53:46 +0530

    new app init

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+import './globals.css';
+import { Link } from "expo-router";
+
+
+
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-5xl text-primary font-bold">Welcome!</Text>
+
+    </View>
+  );
+}
